4ff4526f1e09bddf0a797ffd9e82bf0feed42181
xjzihan <xjjz421@126.com>
2019-06-11 15:54:19 +0800

    进行总结

--- src/main/java/com/xj/classloader/demo2/MyChildrenClass.java
+++ src/main/java/com/xj/classloader/demo2/MyChildrenClass.java
@@ -6,6 +6,10 @@ public class MyChildrenClass extends MyParentClass{
 		System.out.println("Children 无参构造方法 A");
 	}
 	
+	public MyChildrenClass(String name){
+		System.out.println("Children 带一个参构造方法 A");
+	}
+	
 	
 	{ 
 		System.out.println("Children 动态块 B"); 
